5d218850d3fbba62098e35859228fe5849efb595
hugo-esquer <hugo.esquer@laplateforme.io>
2024-01-02 10:51:00 +0100

    job02 fin

---
+++ jour01/job02.py
@@ -0,0 +1,9 @@
+class Operation:
+    def __init__(self, nombre1=5, nombre2=8):
+        self.nombre1 = nombre1
+        self.nombre2 = nombre2
+
+test = Operation()
+
+print(f"Le nombre1 est {test.nombre1}")
+print(f"Le nombre2 est {test.nombre2}")
