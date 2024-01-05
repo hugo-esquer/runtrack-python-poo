a938bd004f96ad8356fa5f7ee63c488aef3556da
hugo-esquer <hugo.esquer@laplateforme.io>
2024-01-05 09:52:12 +0100

    job01 fin

---
+++ jour04/job01.py
@@ -0,0 +1,32 @@
+class Personne:
+    def __init__(self, age=14):
+        self.age = age
+
+    def afficherAge(self):
+        print(self.age)
+
+    def bonjour(self):
+        print("Hello")
+
+    def modifierAge(self, age):
+        if type(age) == int:
+            self.age = age
+    
+class Eleve(Personne):
+    def allerEnCours(self):
+        print("je vais en cours")
+
+    def afficherAge(self):
+        print(f"J'ai {self.age} ans")
+
+class Professeur(Personne):
+    def __init__(self, age, matiereEnseignee):
+        Personne.__init__(self, age)
+        self.matiereEnseignee = matiereEnseignee
+
+    def enseigner(self):
+        print("Le cours va commencer")
+
+jhon = Personne(25)
+cassius = Eleve()
+cassius.afficherAge()
