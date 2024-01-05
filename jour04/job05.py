588681b2344e3ebe1fbd6162d456273d63aff164
hugo-esquer <hugo.esquer@laplateforme.io>
2024-01-05 14:28:51 +0100

    job05 fin

---
+++ jour04/job05.py
@@ -0,0 +1,25 @@
+import math
+class Forme:
+    def aire(self):
+        return 0
+
+class Rectangle(Forme):
+    def __init__(self, largeur, hauteur):
+        self.largeur = largeur
+        self.hauteur = hauteur
+
+    def aire(self):
+        return self.largeur * self.hauteur
+
+class Cercle(Forme):
+    def __init__(self, radius):
+        self.radius = radius
+    
+    def aire(self):
+        return math.pi * self.radius**2
+
+rectangle = Rectangle(26, 9)
+cercle = Cercle(15)
+
+print(rectangle.aire())
+print(cercle.aire())
